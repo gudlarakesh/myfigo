37b438d2c405ef7e93369e0caf980eed3e56a224
gudlarakesh <gudlarakesh1991@gmail.com>
2016-12-04 11:14:16 +0530

    modified view

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
   $('.parallax').parallax();
+  $('#landImage').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa',
+    density: 20000
+  });
   $(".button-collapse").sideNav();
+  $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("slow");
+                $(this).toggleClass('open');
+                // $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("slow");
+                $(this).toggleClass('open');
+                // $('b', this).toggleClass("caret caret-up");                
+            });
+
+
 });
