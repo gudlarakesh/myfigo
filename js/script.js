aa8a0e06cfa7bd65b5ef52b6b6b3e5828bb91c34
gudlarakesh <gudlarakesh1991@gmail.com>
2016-12-04 15:07:51 +0530

    changed landing image in section 2 and wow effects added in products

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#landImage').particleground({
     dotColor: '#5cbdaa',
     lineColor: '#5cbdaa',
-    density: 10000
+    density: 25000
   });
   $(".button-collapse").sideNav();
   $(".dropdown").hover(            
